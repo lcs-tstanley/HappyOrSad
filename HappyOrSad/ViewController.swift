9229a8809ee04a42445ebb81869dc66d63908662
Trent Stanley <tstanley@lcs.on.ca>
2019-11-20 11:32:03 -0500

    Hooked up the view to the controller (outlets and actions created)

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    // Outlets
+    @IBOutlet weak var phraseInputField: UITextField!
+
+    @IBOutlet weak var outputResult: UILabel!
+    
+    // MARK: Methods
+    // Actions go here
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Analyze the text provided
+    @IBAction func analyzeText(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
+    
 }
 
