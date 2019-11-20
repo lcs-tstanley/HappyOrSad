b72ee107c04d325e056d280524d9a4bdd85ea8ed
Trent Stanley <tstanley@lcs.on.ca>
2019-11-20 11:55:16 -0500

    Protected against bad user input

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -26,8 +26,20 @@ class ViewController: UIViewController {
     // Analyze the text provided
     @IBAction func analyzeText(_ sender: Any) {
         
+        //  Clear out the output label from the last time the Analyze button was pressed
+        outputResult.text = ""
         
+        // Guard against no input
+        guard let phraseInput = phraseInputField.text, phraseInput.count > 0 else {
+            outputResult.text = "Please enter a phrase to analyze"
+            return
+        }
         
+        // Make sure the input is not too long
+        if phraseInput.count > 255 {
+            outputResult.text = "Please enter a phrase with 255 characters or less."
+            return
+        }
         
     }
     
