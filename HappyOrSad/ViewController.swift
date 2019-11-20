c3cb42bb69ea6c0a91a5e06ef02b07d2961883a0
Trent Stanley <tstanley@lcs.on.ca>
2019-11-20 11:13:20 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Stanley, Trent on 2019-11-20.
+//  Copyright © 2019 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
